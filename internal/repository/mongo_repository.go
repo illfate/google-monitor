eeab682c2f7390992984591f980828a5a516184a
hontarau <ilya.hontarau@itechart-group.com>
2023-02-04 17:03:44 +0100

    Add mongo repository

---
+++ internal/repository/mongo_repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/illfate/google-monitor/internal/monitor"
+)
+
+type Mongo struct {
+	collection *mongo.Collection
+}
+
+func NewMongo(collection *mongo.Collection) *Mongo {
+	return &Mongo{
+		collection: collection,
+	}
+}
+
+func (m *Mongo) InsertRequestRes(ctx context.Context, res monitor.RequestResult) error {
+	_, err := m.collection.InsertOne(ctx, bson.M{"code": res.Code})
+	if err != nil {
+		return fmt.Errorf("failed to insert res: %w", err)
+	}
+	return nil
+}
